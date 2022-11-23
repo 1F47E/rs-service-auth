b7a1b1290ec163cadc1543e2188a96163e0ef9b3
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2022-11-23 16:32:49 +0800

    User struct + json

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
 #[macro_use] extern crate rocket;
+use rocket::serde::json::Json;
+use rocket::serde::{Serialize, Deserialize};
 
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
+#[serde(crate = "rocket::serde")]
+struct User {
+    // #[field(validate = len(..30))]
+    // pub room: String,
+    pub id: u32,
+    #[field(validate = len(..3))]
+    pub username: String,
+    #[field(validate = len(..8))]
+    pub password: String,
+}
+
+impl User {
+    pub fn new(id: u32, username: String, password: String) -> Self {
+        User { id, username, password }
+    }
+    pub fn from(id: usize) -> Self {
+        let username = String::from("test");
+        let password = String::from("test");
+        User::new(id as u32, username, password)
+    }
+    
+    // pub fn Deserialize(data: String) -> Self {
+    //     let user: User = serde_json::from_str(&data).unwrap();
+    //     user
+    // }
+
+    // pub fn Deserialize(&self) -> String {
+    //     let json = serde_json::to_string(&self).unwrap();
+    //     json
+    // }
+}
 
 // ROUTES
 #[get("/")]
@@ -12,17 +47,48 @@ fn ping() -> &'static str {
     "pong"
 }
 
-struct User {
-    user_email: String,
-    user_password: String,
+#[get("/<id>")]
+fn user(id: usize) -> Json<User> {
+    let user_from_id = User::from(id);
+    // let user = User {
+    //     id: id as u64,
+    //     login: String::from("test"),
+    //     password: String::from("test"),
+    // };
+    // let user_from_id = User
+    /* ... */
+    Json(user_from_id)
 }
 
 
-
-#[get("/signin")]
-fn sign_in() -> &'static str {
-    "Sign in"
+// Using format = json means that any request that doesn’t specify “application/json”
+// as its Content-Type header value will not be routed to the handler.
+#[post("/", format = "json", data = "<creds>")]
+fn sign_in(creds: Json<User>) -> Json<User> {
+    println!("{:#?}", creds);
+    // let user = User {
+    //     id: 1,
+    //     username: creds.username,
+    //     password: creds.password,
+    // };
+    // Json(creds)
+    creds
 }
+
+
+// add post handler with json
+// #[post("/", format = "json", data = "<user_input>")]
+// fn sign_in(user_input: Json<User>) -> String {
+//     // return out json data pretty
+//     println!("{:#?}", user_input);
+//     // Json(data)
+//     user_input
+// }
+
+// #[get("/signin")]
+// fn sign_in() -> &'static str {
+//     "Sign in"
+// }
 
 #[get("/signout")]
 fn sign_out() -> &'static str {
@@ -44,4 +110,5 @@ fn rocket() -> _ {
         .mount("/signin", routes![sign_in])
         .mount("/signout", routes![sign_out])
         .mount("/refresh", routes![refresh])
+        .mount("/user", routes![user])
 }
