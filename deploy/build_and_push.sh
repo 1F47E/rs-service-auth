741c80917c62158af36ec0a642844ae66ebfa03d
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2022-11-25 20:40:37 +0800

    deploy bugfixing, wip

--- deploy/build_and_push.sh
+++ deploy/build_and_push.sh
@@ -1,4 +1,4 @@
-docker build --platform linux/amd64 -f deploy/Dockerfile -t rust-service-auth .
+docker build --platform linux/amd64 -f Dockerfile -t rust-service-auth .
 # docker tag rust-service-auth docker.io/kkkasparagus/rust-service-auth:latest 
 # docker push docker.io/kkkasparagus/rust-service-auth:latest
 
