5dc94bb5000377aefbc75221387eba1a232d352e
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2022-11-26 23:09:52 +0800

    hashed pwd

--- src/user.rs
+++ src/user.rs
@@ -1,6 +1,9 @@
 use rocket::form::FromForm;
 use rocket::serde::{Deserialize, Serialize};
 
+use pwhash::bcrypt;
+use names::Generator;
+
 #[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
 #[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
 #[serde(crate = "rocket::serde")]
@@ -18,18 +21,44 @@ pub struct User {
     pub id: u64,
     pub username: String,
     pub password: String,
+    pub fullname: String,
 }
 
 // TODO: hash password 
 impl User {
     pub fn new(id: u64, username: String, password: String) -> Self {
+        let hashed_pwd = bcrypt::hash(password).unwrap();
+        let fullname = User::generate_random_name();
         User {
             id,
             username,
-            password,
+            password:hashed_pwd,
+            fullname
         }
     }
+
+    fn uppercase_first_letter(s: &str) -> String {
+        let mut c = s.chars();
+        match c.next() {
+            None => String::new(),
+            Some(f) => f.to_uppercase().chain(c).collect(),
+        }
+    }
+
+    pub fn generate_random_name() -> String {
+        // generate random name (slow!)
+        let mut generator = Generator::default();
+        let gen_name = generator.next().unwrap();
+        let names = gen_name.replace("-", " ");
+        // split the name into first and last and capitalize the first letter
+        let mut split = names.split_whitespace();
+        let first = split.next().unwrap();
+        let last = split.next().unwrap();
+        let first_name = User::uppercase_first_letter(first);
+        let last_name = User::uppercase_first_letter(last);
+        format!("{} {}", first_name, last_name)
+    }
     pub fn check_pwd(&self, password: &str) -> bool {
-        self.password == password
+        bcrypt::verify(password, &self.password)
     }
 }
